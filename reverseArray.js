cf399429ebd94b259d4a954b231dea06a1dcfae0
mohindar99 <d.mohindar1999@gmail.com>
2023-05-15 10:09:57 +0530

    reverseArray

---
+++ reverseArray.js
@@ -0,0 +1,22 @@
+// reversing an array using recurssion
+
+// let data = [2, 6, 1, 10, 98];
+// let temp;
+
+// function customReverse(data, start, end) {
+//  console.log(data);
+//   if (start <= end) {
+//     temp = data[start];
+//     data[start] = data[end];
+//     data[end] = temp;
+//     customReverse(data, start + 1, end - 1);
+//   }
+// }
+// customReverse(data, 0, data.length - 1);
+
+
+// default function for reversing an array
+
+let item = [2, 3, 41, 12, 23];
+let data1 = item.reverse();
+console.log(data1);
